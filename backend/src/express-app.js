e953f18adc2e6f65328c483f829317d4b925dc82
Mohammad Atif <mhdatif0511@gmail.com>
2025-12-31 05:22:36 +0530

    fix: configure CORS to allow requests from the frontend URL

--- backend/src/express-app.js
+++ backend/src/express-app.js
@@ -10,7 +10,12 @@ export const expressApp = (app) => {
 
     app.use(express.json({ limit: '1mb' }));
     app.use(express.urlencoded({ extended: true, limit: '1mb' }));
-    app.use(cors());
+    app.use(cors(
+        {
+            origin: FRONTEND_URL,
+            credentials: true,
+        }
+    ));
     app.use(express.static("public"));
     app.use(cookieParser());
 
@@ -20,7 +25,4 @@ export const expressApp = (app) => {
 }
 
 
-// {
-//     origin: FRONTEND_URL,
-//     credentials: true,
-// }
+
